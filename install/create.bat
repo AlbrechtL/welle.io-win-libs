ced0f0d5bc9ff01e575eea2526d42a6ac8763435
Albrecht Lohofener <albrechtloh@gmx.de>
2017-10-17 20:04:04 +0200

    Some improvements

--- install/create.bat
+++ install/create.bat
@@ -5,8 +5,12 @@ for /f "tokens=2 delims==" %%G in ('wmic os get localdatetime /value') do set da
 set year=%datetime:~0,4%
 set month=%datetime:~4,2%
 set day=%datetime:~6,2%
-set current_date= %year%%month%%day%
+set current_date=%year%%month%%day%
+for /f %%i in ('git -C %1 rev-parse --short HEAD') do set git_hash=%%i
 
-C:\Qt\Tools\QtInstallerFramework\2.0\bin\binarycreator.exe --offline-only --config config/config.xml --packages packages %current_date%_welle-io-setup.exe
+set filename=%current_date%_%git_hash%_welle-io-setup.exe
+
+echo Creating %filename%
+C:\Qt\Tools\QtInstallerFramework\2.0\bin\binarycreator.exe --offline-only --config config/config.xml --packages packages %filename%
 
 pause
